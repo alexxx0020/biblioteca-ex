ab89320632b4917579089498b6c2a770fb64d35b
alexxx0020 <alessandro.longo@finconsgroup.com>
2025-05-06 12:29:11 +0200

    Create ErrorResponse.java

---
+++ src/main/java/com/example/biblioteca_ex/response/ErrorResponse.java
@@ -0,0 +1,12 @@
+package com.example.biblioteca_ex.response;
+
+import java.time.LocalDateTime;
+
+public class ErrorResponse {
+
+    private String message;
+    private int status;
+    private LocalDateTime date;
+
+
+}
